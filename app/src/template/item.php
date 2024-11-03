4573f6350e75191b66982fa491a2cf93c111a6c5
ParkerKaiMalachai <irinkademchuuuuuuuk@gmail.com>
2024-11-03 21:12:02 +0300

    created views and templates

---
+++ app/src/template/item.php
@@ -0,0 +1,11 @@
+<?php 
+
+declare(strict_types=1);
+
+?>
+
+<li>
+    <?php echo "name: \n" . $value["name"] ?>
+    <?php echo "price: \n" . $value["price"] ?>
+    <?php echo "quantity: \n" . $value["quantity"] ?>
+</li>
